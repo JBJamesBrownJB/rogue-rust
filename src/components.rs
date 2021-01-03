a013d945a36bb0dfb8f0d6406adf76ec237a1a68
JB <JamesKinnahBrown@gmail.com>
2021-01-03 15:11:36 +0000

    Synced cose for chap 5

--- src/components.rs
+++ src/components.rs
@@ -1,6 +1,6 @@
 use specs::prelude::*;
 use specs_derive::*;
-use rltk::{RGB, Rltk, Point};
+use rltk::{RGB};
 
 #[derive(Component)]
 pub struct Position {
@@ -16,7 +16,11 @@ pub struct Renderable {
 }
 
 #[derive(Component, Debug)]
+pub struct Player {}
+
+#[derive(Component)]
 pub struct Viewshed {
-    pub visible_tiles: Vec<Point>,
-    pub range: i32
+    pub visible_tiles : Vec<rltk::Point>,
+    pub range : i32,
+    pub dirty : bool
 }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,39 @@
-mod map;
-mod player;
-mod components;
-mod rect;
-mod visibility_system;
-
-use rltk::*;
+use rltk::{GameState, Rltk, RGB};
 use specs::prelude::*;
-use std::cmp::{max, min};
-use std::borrow::{Borrow};
-use map::*;
+mod components;
+pub use components::*;
+mod map;
+pub use map::*;
+mod player;
 use player::*;
-use components::*;
-use crate::visibility_system::VisibilitySystem;
+mod rect;
+pub use rect::Rect;
+mod visibility_system;
+use visibility_system::VisibilitySystem;
 
 pub struct State {
-    ecs: World
+    pub ecs: World
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut vis = VisibilitySystem{};
+        vis.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
 }
 
 impl GameState for State {
-    fn tick(&mut self, ctx: &mut Rltk) {
+    fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
-        ctx.print(0, 0, ctx.fps);
 
-        self.run_systems();
         player_input(self, ctx);
+        self.run_systems();
+
+        draw_map(&self.ecs, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
-
-        let mut map = self.ecs.fetch::<Map>();
-        map.draw_map(ctx);
 
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
@@ -37,47 +41,34 @@ impl GameState for State {
     }
 }
 
-impl State {
-    fn run_systems(&mut self) {
-        let mut vis_sys = VisibilitySystem{};
-        vis_sys.run_now(&self.ecs);
-        self.ecs.maintain();
-    }
-}
-
 fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
     let mut gs = State {
         ecs: World::new()
     };
-
-    let map = Map::new_map_rooms_corridors();
-    let (player_x, player_y) = map.rooms[0].center();
-    gs.ecs.insert(map);
-
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
     gs.ecs.register::<Viewshed>();
 
+    let map : Map = Map::new_map_rooms_and_corridors();
+    let (player_x, player_y) = map.rooms[0].center();
+    gs.ecs.insert(map);
+
     gs.ecs
         .create_entity()
         .with(Position { x: player_x, y: player_y })
-        .with(Player {})
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
-        .with(Viewshed {
-            visible_tiles: Vec::new(),
-            range: 8,
-        })
+        .with(Player{})
+        .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
         .build();
-
-
-    let context = RltkBuilder::simple80x50()
-        .with_title("Roguelike Tutorial")
-        .build()?;
 
     rltk::main_loop(context, gs)
 }
